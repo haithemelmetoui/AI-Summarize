95d9e59e8704886d17f1e101716b3d2906efdeb0
haithemelmetoui <elmetoui.haithem@gmail.com>
2024-11-04 23:45:40 +0100

    creating store with redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();  
   return (
     <div>Demo</div>
   )
